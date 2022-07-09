a757fcc494a801ac57ebe80ecc3df92e406f2a99
Shivangam Soni <shivangamsoni99@gmail.com>
2022-07-09 20:05:39 +0530

    Add: / Redirect to /rent Add: Netlify.toml file

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,12 +1,13 @@
-import { Routes as RouterRoutes, Route } from "react-router-dom";
-import NotFound from "./pages/NotFound/NotFound";
+import { Routes as RouterRoutes, Route, Navigate } from "react-router-dom";
 
 import Rent from "./pages/Rent/Rent";
+import NotFound from "./pages/NotFound/NotFound";
 
 const Routes = () => {
   return (
     <main>
       <RouterRoutes>
+        <Route path="/" element={<Navigate to="/rent" />} />
         <Route path="/rent" element={<Rent />} />
         <Route path="*" element={<NotFound />} />
       </RouterRoutes>
